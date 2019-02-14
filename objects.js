18e9df23c08e4bca9d27fcad5b967de21387c596
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 18:12:29 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = {
   'Panic': 'Porch Song', 
   'The Dead': 'Cassidy', 
@@ -11,3 +12,6 @@ function removeFromPlaylist(obj, key) {
   delete obj[key];
   return obj;
 }
+=======
+var playlist = {Panic: 'Porch Song', The Dead: 'Cassidy', Allman Brothers: 'Stormy Monday'};
+>>>>>>> d7d62d2ac34286747dd5a3bf5362bd9e828de854
